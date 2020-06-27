3739ab51c1fcb98d627609c52e71d5e57f8eb9fd
jonathan-mendes <jonathan_mendes96@outlook.com>
2020-06-27 00:08:49 -0300

    teste

--- controllers/produtos-controller.js
+++ controllers/produtos-controller.js
@@ -10,12 +10,6 @@ exports.getProdutos = async (req, res, next) => {
                     id: prod.id,
                     nome: prod.nome,
                     preco: prod.preco,
-                    // imagem_produto: prod.imagem_produto,
-                    request: {
-                        tipo: 'GET',
-                        descricao: 'Retorna os detalhes de um produto específico',
-                        url: process.env.URL_API + 'produtos/' + prod.id_produto
-                    }
                 }
             })
         }
@@ -27,25 +21,18 @@ exports.getProdutos = async (req, res, next) => {
 
 exports.postProduto = (req, res, next) => {
     try {
-        const query = 'INSERT INTO produtos (nome, preco, imagem_produto) VALUES (?,?,?)';
+        const query = 'INSERT INTO produtos (nome, preco) VALUES (?,?)';
         const result = mysql.execute(query, [
             req.body.nome,
             req.body.preco,
-            req.file.path
         ]);
 
         const response = {
             mensagem: 'Produto inserido com sucesso',
             produtoCriado: {
-                id_produto: result.insertId,
+                id: result.insertId,
                 nome: req.body.nome,
                 preco: req.body.preco,
-                imagem_produto: req.file.path,
-                request: {
-                    tipo: 'GET',
-                    descricao: 'Retorna todos os produtos',
-                    url: process.env.URL_API + 'produtos'
-                }
             }
         }
         return res.status(201).send(response);
@@ -56,8 +43,8 @@ exports.postProduto = (req, res, next) => {
 
 exports.getUmProduto = async (req, res, next)=> {
     try {
-        const query = 'SELECT * FROM produtos WHERE id_produto = ?;';
-        const result = await mysql.execute(query, [req.params.id_produto]);
+        const query = 'SELECT * FROM produtos WHERE id = ?;';
+        const result = await mysql.execute(query, [req.params.id]);
 
         if (result.length == 0) {
             return res.status(404).send({
@@ -66,15 +53,9 @@ exports.getUmProduto = async (req, res, next)=> {
         }
         const response = {
             produto: {
-                id_produto: result[0].id_produto,
+                id: result[0].id,
                 nome: result[0].nome,
                 preco: result[0].preco,
-                imagem_produto: result[0].imagem_produto,
-                request: {
-                    tipo: 'GET',
-                    descricao: 'Retorna todos os produtos',
-                    url: process.env.URL_API + 'produtos'
-                }
             }
         }
         return res.status(200).send(response);
@@ -89,22 +70,22 @@ exports.updateProduto = async (req, res, next) => {
         const query = ` UPDATE produtos
                            SET nome        = ?,
                                preco       = ?
-                         WHERE id_produto  = ?`;
+                         WHERE id  = ?`;
         await mysql.execute(query, [
             req.body.nome,
             req.body.preco,
-            req.body.id_produto
+            req.body.id
         ]);
         const response = {
             mensagem: 'Produto atualizado com sucesso',
             produtoAtualizado: {
-                id_produto: req.body.id_produto,
+                id: req.body.id,
                 nome: req.body.nome,
                 preco: req.body.preco,
                 request: {
                     tipo: 'GET',
                     descricao: 'Retorna os detalhes de um produto específico',
-                    url: process.env.URL_API + 'produtos/' + req.body.id_produto
+                    // url: process.env.URL_API + 'produtos/' + req.body.id
                 }
             }
         }
@@ -117,14 +98,14 @@ exports.updateProduto = async (req, res, next) => {
 exports.deleteProduto = async (req, res, next) => {
     try {
         const query = `DELETE FROM produtos WHERE id_produto = ?`;
-        await mysql.execute(query, [req.body.id_produto]);
+        await mysql.execute(query, [req.body.id]);
 
         const response = {
             mensagem: 'Produto removido com sucesso',
             request: {
                 tipo: 'POST',
                 descricao: 'Insere um produto',
-                url: process.env.URL_API + 'produtos',
+                // url: process.env.URL_API + 'produtos',
                 body: {
                     nome: 'String',
                     preco: 'Number'
